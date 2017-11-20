fc937e206c1c6a47671ed82572ee279036524b63
jimmyyou <jieyou@umich.edu>
2017-11-20 13:53:33 -0500

    Commit before adding multi-host support and only transmitting the needed partitions

---
+++ src/main/java/gaiaframework/transmission/ForwardingAgent.java
@@ -0,0 +1,9 @@
+package gaiaframework.transmission;
+
+// Forwards the data to the specific destination host inside the DataCenter
+// Each Data Center only has one ForwardingAgent
+
+public class ForwardingAgent {
+
+
+}
