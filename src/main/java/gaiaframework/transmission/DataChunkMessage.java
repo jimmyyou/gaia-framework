cf795c97f60e885a64bd4a63b9461a8275d227d5
Jimmy <jimmy.jie.you@gmail.com>
2018-09-16 14:27:11 -0400

    Clean up: remove DataChunkHeader.java

--- src/main/java/gaiaframework/transmission/DataChunkMessage.java
+++ src/main/java/gaiaframework/transmission/DataChunkMessage.java
@@ -6,54 +6,62 @@ import java.io.Serializable;
 
 public class DataChunkMessage implements Serializable {
 
-    private static final long serialVersionUID = 759099461767050471L;
+//    private static final long serialVersionUID = 759099461767050471L;
 
-    DataChunkHeader header;
+//    DataChunkHeader header;
+
+    // Field previously from header
+    String filename;
+    String destURL;
+    String blockID;
+    long chunkStartIndex;
+    long totalBlockLength;
+    long totalFileLength;
 
     byte[] data;
 
     public DataChunkMessage(String filename, String destIP, String blockID, long startIndex, long totalBlockLength, long totalFileLength, byte[] data) {
-        header = new DataChunkHeader(filename, destIP, blockID, startIndex, totalBlockLength, totalFileLength);
+//        header = new DataChunkHeader(filename, destIP, blockID, startIndex, totalBlockLength, totalFileLength);
+        this.filename = filename;
+        this.destURL = destIP;
+        this.blockID = blockID;
+        this.chunkStartIndex = startIndex;
+        this.totalBlockLength = totalBlockLength;
+        this.totalFileLength = totalFileLength;
+
         this.data = data;
     }
 
-    public String getDestURL() { return header.getDestURL(); }
+    public String getDestURL() { return this.destURL; }
 
-    public String getFilename() {
-        return header.getFilename();
-    }
+    public String getFilename() { return this.filename; }
 
-    public String getBlockId() { return header.blockID; }
+    public String getBlockId() { return this.blockID; }
 
-    public long getStartIndex() { return header.getChunkStartIndex(); }
+    public long getStartIndex() { return this.chunkStartIndex; }
 
-    public long getTotalBlockLength() { return header.getTotalBlockLength(); }
+    public long getTotalBlockLength() { return this.totalBlockLength; }
 
     @Deprecated
-    public long getTotalFileLength() { return header.totalFileLength; }
+    public long getTotalFileLength() { return this.totalFileLength; }
 
     public byte[] getData() {
         return data;
     }
 
-    public DataChunkHeader getHeader() {
-        return header;
-    }
-
-    public void setHeader(DataChunkHeader header) {
-        this.header = header;
-    }
-
     // for debug purpose only
     public void appendToFilename(String appendix) {
 //        filename.concat(appendix);
-        header.filename = header.filename + appendix;
+        this.filename = this.filename + appendix;
     }
 
     @Override
     public String toString() {
         return "DataChunkMessage{" +
-                "header=" + header +
+                "header=" + "filename='" + filename + '\'' +
+                ", destURL='" + destURL + '\'' +
+                ", chunkStartIndex=" + chunkStartIndex +
+                ", totalBlockLength=" + totalBlockLength +
                 ", data size=" + data.length +
                 '}';
     }
