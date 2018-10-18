b84b8827f79e6b55db14a8b8df3df72a282f69f9
Jimmy <jimmy.jie.you@gmail.com>
2018-10-17 23:04:01 -0400

    Increase some parameters

--- src/main/java/gaiaframework/util/Constants.java
+++ src/main/java/gaiaframework/util/Constants.java
@@ -56,11 +56,11 @@ public class Constants {
     public static final int SENDER_QUEUE_LENGTH = 1000;
     public static final String SCHEDULER_NAME_GAIA = "gaia";
     //    public static final int HTTP_CHUNKSIZE = 8192;
-    public static final int HTTP_CHUNKSIZE = 32768;
+    public static final int HTTP_CHUNKSIZE = 327680;
     public static final int DEFAULT_HTTP_SERVER_PORT = 20020;
     public static final int FETCHER_QUEUE_LENGTH = 100; // TODO: check other queue length
-    public static final int FETCHER_THREADS = 4;
-    public static final int WRITER_THREADS = 8;
+    public static final int FETCHER_THREADS = 20;
+    public static final int WRITER_THREADS = 20;
 
     public static HashMap<String, String> node_id_to_trace_id;
 
